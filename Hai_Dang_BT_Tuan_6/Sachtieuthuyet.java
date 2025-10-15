d506b503596b78b04b3d144f1c97f8791491d0f4
haidang20081811-haidang <haidang20081811@gmail.com>
2025-10-15 19:32:48 +0700

    tranlehaidang

---
+++ Hai_Dang_BT_Tuan_6/Sachtieuthuyet.java
@@ -0,0 +1,19 @@
+package Hai_Dang_BT_Tuan_5;
+
+public class Sachtieuthuyet extends sach {
+    private String theLoai;
+    private boolean laSachSeries;
+
+    public Sachtieuthuyet(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, String theLoai, boolean laSachSeries) {
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        this.theLoai = theLoai;
+        this.laSachSeries = laSachSeries;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+               ", The loai: " + theLoai + 
+               ", La series: " + (laSachSeries ? "Co" : "Khong");
+    }
+}
